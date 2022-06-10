38f468ce25afc8db6f1a0026e84b72c4b4c7fd87
rohitpatil8046 <106608815+rohitpatil8046@users.noreply.github.com>
2022-06-10 18:32:54 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] age = { 12, 33, 45, 34, 15, 34, 66, 78, 65, 89, 87, 56, 55, 90, 87 };
+            var a = from i in age where i > 20 orderby i select i;
+
+            foreach (int item in a)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
