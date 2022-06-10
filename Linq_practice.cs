eb766ca807b88e4fe4da0c3c9d6fc07e5b9a59c8
rohitpatil8046 <106608815+rohitpatil8046@users.noreply.github.com>
2022-06-10 18:41:51 +0530

    Add files via upload

---
+++ Linq_practice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqDemo
+{
+    internal class Linq_practice
+    {
+        static void Main(string[] args)
+        {
+
+            string[] names = { "Rohit","Amit","Swapnil","Karan","Saurav" };
+            var a = from name in names where name.Contains("a") select name;
+
+            foreach (string item in a)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
